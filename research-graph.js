4eac656d5d76bf3604950772ebaa93440e7b8f49
rahulbhaumik010 <rbhaumik226@gmail.com>
2026-05-21 20:06:45 +0200

    Update research-graph.js

--- research-graph.js
+++ research-graph.js
@@ -82,19 +82,14 @@ if (graphContainer) {
           ctx.fill();
         })
         .onNodeClick(node => {
+  if (node.id === "rahul-research") {
+    window.location.href = "graph3d.html";
+    return;
+  }
+
   showNodeDetails(node);
   Graph.centerAt(node.x, node.y, 700);
   Graph.zoom(1.7, 700);
-})
-.onNodeRightClick(node => {
-  if (node.id === "rahul-research") {
-    window.location.href = "graph3d.html";
-  }
-})
-.onNodeDoubleClick(node => {
-  if (node.id === "rahul-research") {
-    window.location.href = "graph3d.html";
-  }
 });
 
       Graph.d3Force("charge").strength(-120);
